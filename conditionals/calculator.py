b1d7d367c5e85ccf313e4e273d08c5e373eff87f
saiganesh <saidinavahi42@gmail.com>
2026-07-24 23:59:31 +0530

    day3 conditions

---
+++ conditionals/calculator.py
@@ -0,0 +1,10 @@
+#input samll, meduim,large 
+#s 5,m 15,l 45
+
+cup = input(f'select cup size:{cup}').lower()
+print(f"u r cup size is: {cup}")
+if s==5 or l==15 or m==45:
+    print(f'great choosie :{cup}')
+else:
+    print(f'give cup size is unknow')
+    
