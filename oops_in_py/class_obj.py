66d9d1a77e4612d0e8a0e6a24e58ebdca9b8b1f1
saiganesh <saidinavahi42@gmail.com>
2026-08-11 19:56:02 +0530

    started oops

---
+++ oops_in_py/class_obj.py
@@ -0,0 +1,11 @@
+# very basic class and obj
+#py is a obj based lang every this is A obj
+
+class Ganesh:
+    pass
+
+name = Ganesh()
+print(type(name))
+print(type(name) is Ganesh)# name is a class which is made with ganesh class
+# with using is we check is the obj belong to this class or not 
+# obj,true
